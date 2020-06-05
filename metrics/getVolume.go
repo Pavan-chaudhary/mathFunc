9e1f3b8c6640362bad3f776bd5c689a2a11f1163
Pavan <pavanchaudhary153@gmail.com>
2020-06-05 16:02:12 +0530

    bugFixes

--- metrics/getVolume.go
+++ metrics/getVolume.go
@@ -2,12 +2,12 @@ package metrics
 
 import "math"
 
-func GetSphereVolume(radius float64) vol float64{
-    vol := (4 * math.Pi * radius * radius * radius)/3
+func GetSphereVolume(radius float64) (vol float64) {
+    vol = (4 * math.Pi * radius * radius * radius)/3
     return
 }
 
-func GetCylinderVolume(radius float64, height float64) vol float64{
-    vol := 2 * math.Pi * radius * height
+func GetCylinderVolume(radius float64, height float64) (vol float64){
+    vol = 2 * math.Pi* radius * height
     return
 }
